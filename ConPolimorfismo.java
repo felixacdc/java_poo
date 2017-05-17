04eb5f92df7b28a2ad0d14b9ede5647e04bb9399
felixacdc <pfmata01@gmail.com>
2017-05-17 15:25:38 -0600

    [add] Polimorfismo parte 2

---
+++ ConPolimorfismo.java
@@ -0,0 +1,80 @@
+import java.lang.Math;
+
+public class Show
+{
+    public static void main(String[] args)
+    {
+        //declaro los objetos que heredan de tipo vehículo
+        Auto auto = new Auto();
+        Moto moto= new Moto();
+        Bus bus = new Bus();
+        Camion camion = new Camion();
+        
+        //declaro el objeto peaje para identificar y cobrar el peaje de acuerdo al tipo de vehículo
+        Peaje peaje= new Peaje();
+        //cobrar peaje auto
+        peaje.cobrarPeajeAuto(auto);
+        System.out.println();
+        //cobrar peaje bus
+        peaje.cobrarPeajeBus(bus);
+        System.out.println();
+        //cobrar peaje camion
+        peaje.cobrarPeajeCamion(camion);
+        System.out.println();
+        //cobrar peaje moto
+        peaje.cobrarPeajeMoto(moto);
+    }
+}
+
+//clase padre Vehiculo (Clase base)
+public class Vehiculo {
+    public void mostrarTipoVehiculo(){
+        System.out.println("Soy un Vehiculo");
+        System.out.println("Cobrar peaje Vheículo");
+    }
+}
+
+//clase hija Auto (SubClase)
+public class Auto extends Vehiculo {
+    @Override
+    public void mostrarTipoVehiculo() {
+        System.out.println("Soy un auto");
+        System.out.println("Cobrar peaje auto");
+    }
+}
+
+//clase hija moto (SubClase)
+public class Moto extends Vehiculo {
+    @Override
+    public void mostrarTipoVehiculo() {
+        System.out.println("Soy una moto");
+        System.out.println("Cobrar peaje moto");
+    }
+}
+
+//clase hija camion (SubClase)
+public class Camion extends Vehiculo {
+    @Override
+    public void mostrarTipoVehiculo() {
+        System.out.println("Soy un camion");
+        System.out.println("Cobrar peaje camion");
+    }
+}
+
+//clase hija bus (SubClase)
+public class Bus extends Vehiculo {
+    @Override
+    public void mostrarTipoVehiculo() {
+        System.out.println("Soy un bus");
+        System.out.println("Cobrar peaje bus");
+    }
+}
+
+public class Peaje {
+    
+    //método para cobrar peaje para cualquier vehículo
+    public void cobrarPeaje(Vehiculo vehiculo){
+        vehiculo.mostrarTipoVehiculo();
+    }   
+
+}
